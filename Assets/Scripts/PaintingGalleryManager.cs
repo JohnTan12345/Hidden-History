0cd526589f72e86ab0c7b3a16d50efb8676cb7bf
JohnTan12345 <s10259175@connect.np.edu.sg>
2025-12-01 15:41:58 +0800

    Started reworking the painting gallery manager

---
+++ Assets/Scripts/PaintingGalleryManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class PaintingGalleryManager : MonoBehaviour
+{
+    public List<PaintingData> paintings = new List<PaintingData>();
+}
