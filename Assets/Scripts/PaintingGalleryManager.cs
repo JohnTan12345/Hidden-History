0d263604b2d59b8567d35bbcbffa6b3048b6e0d8
JohnTan12345 <s10259175@connect.np.edu.sg>
2025-12-03 22:47:49 +0800

    Added Artifact Page Framework

--- Assets/Scripts/PaintingGalleryManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using System.Collections.Generic;
-
-public class PaintingGalleryManager : MonoBehaviour
-{
-    public List<PaintingData> paintings = new List<PaintingData>();
-}
